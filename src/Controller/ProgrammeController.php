78b2aad3da737e75db29614924836cf6153364cd
AnotheerDev <leger.julien94@gmail.com>
2023-07-11 11:04:55 +0200

    Adding controllers and templates

---
+++ src/Controller/ProgrammeController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ProgrammeController extends AbstractController
+{
+    #[Route('/programme', name: 'app_programme')]
+    public function index(): Response
+    {
+        return $this->render('programme/index.html.twig', [
+            'controller_name' => 'ProgrammeController',
+        ]);
+    }
+}
