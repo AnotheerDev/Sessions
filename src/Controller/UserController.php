f5aac4735671a05aef637e6846e471caf0c758bc
AnotheerDev <leger.julien94@gmail.com>
2023-07-11 16:48:48 +0200

    add function ShowListerUser & showProfile

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -2,9 +2,12 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\User;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class UserController extends AbstractController
 {
@@ -16,9 +19,22 @@ class UserController extends AbstractController
         ]);
     }
 
-    #[Route('/user/show', name: 'app_showProfile')]
-    public function showProfile(): Response
+
+    #[Route('/user/listUser', name: 'app_showListUser')]
+    public function showListUser(ManagerRegistry $doctrine): Response
     {
-        return $this->render('user/show.html.twig', []);
+        $users = $doctrine->getRepository(User::class)->findAll();
+        return $this->render('user/listUser.html.twig', [
+            'users' => $users
+        ]);
+    }
+
+
+    #[Route('/user/{id}', name: 'app_showProfile')]
+    public function showProfile(User $user): Response
+    {
+        return $this->render('user/show.html.twig', [
+            'user' => $user
+        ]);
     }
 }
