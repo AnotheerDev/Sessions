b94c0e9a4870acccf44909cdb38c96dead504ead
AnotheerDev <leger.julien94@gmail.com>
2023-07-12 16:39:39 +0200

    adding function and rooting for session view

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -2,17 +2,27 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Session;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class SessionController extends AbstractController
 {
     #[Route('/session', name: 'app_session')]
-    public function index(): Response
+    public function index(ManagerRegistry $doctrine): Response
     {
+        $sessions = $doctrine->getRepository(Session::class)->findAll();
         return $this->render('session/index.html.twig', [
-            'controller_name' => 'SessionController',
+            'sessions' => $sessions
+        ]);
+    }
+
+    #[Route('/showSession/{id}', name: 'app_showSession')]
+    public function show(): Response
+    {
+        return $this->render('session/showSession.html.twig', [
         ]);
     }
 }
