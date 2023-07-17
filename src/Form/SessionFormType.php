3909d07249185cb23688e1c701d57438dabd9c2e
AnotheerDev <leger.julien94@gmail.com>
2023-07-17 16:28:43 +0200

    add formType session

---
+++ src/Form/SessionFormType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Session;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class SessionFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('sessionName')
+            ->add('nbPlace')
+            ->add('startSession')
+            ->add('endSession')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Session::class,
+        ]);
+    }
+}
