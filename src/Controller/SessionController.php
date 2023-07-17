3b6b72da486e30d9a2a4929e7c1336d56b5488ae
AnotheerDev <leger.julien94@gmail.com>
2023-07-17 17:00:51 +0200

    add function add

--- src/Controller/SessionController.php
+++ src/Controller/SessionController.php
@@ -3,8 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Session;
+use App\Form\SessionFormType;
 use App\Repository\SessionRepository;
 use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -19,6 +21,37 @@ class SessionController extends AbstractController
             'sessions' => $sessions
         ]);
     }
+
+
+    #[Route('/session/ajoutSession', name: 'ajout_session')]
+    #[Route('/session/{id}/edit', name: 'edit_session')]
+    public function add(ManagerRegistry $doctrine, session $session = null, Request $request): Response
+    {
+
+        if(!$session) {
+            $session = new session();
+        }
+
+        $form = $this->createForm(SessionFormType::class, $session);
+        $form->handleRequest($request); 
+
+        if($form->isSubmitted() && $form->isValid()){
+            $session = $form->getData();
+            $entityManager = $doctrine->getManager();
+            //equivalent prepare request pour éviter les failles SQL
+            $entityManager->persist($session);
+            // insert into (execute)
+            $entityManager->flush();
+
+            return $this->redirectToRoute('app_session');
+        }
+        //vue pour affiche le formaulaire d'ajout 
+        return $this->render('session/ajoutSession.html.twig', [
+            'formAddSession' => $form->createView(),
+            'edit' => $session->getId(),
+        ]);
+    }
+
 
     
     #[Route('/showSession/{id}', name: 'app_showSession')]
