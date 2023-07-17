337f50c9e94d4ed39ea2801427e737fc1e8de68f
AnotheerDev <leger.julien94@gmail.com>
2023-07-17 12:02:39 +0200

    add function index

--- src/Controller/FormationController.php
+++ src/Controller/FormationController.php
@@ -2,17 +2,20 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Formation;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormationController extends AbstractController
 {
     #[Route('/formation', name: 'app_formation')]
-    public function index(): Response
+    public function index( ManagerRegistry $doctrine): Response
     {
+        $formations = $doctrine->getRepository( Formation::class)->findBy([], ["name" => "ASC"]);
         return $this->render('formation/index.html.twig', [
-            'controller_name' => 'FormationController',
+            'formations' => $formations,
         ]);
     }
 }
