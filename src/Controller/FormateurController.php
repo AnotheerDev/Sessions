ac08130217228ed8a130fe0e7b818e7bb966adff
AnotheerDev <leger.julien94@gmail.com>
2023-07-21 14:41:55 +0200

    add FormateurController

---
+++ src/Controller/FormateurController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class FormateurController extends AbstractController
+{
+    #[Route('/formateur', name: 'app_formateur')]
+    public function index(): Response
+    {
+        return $this->render('formateur/index.html.twig', [
+            'controller_name' => 'FormateurController',
+        ]);
+    }
+}
