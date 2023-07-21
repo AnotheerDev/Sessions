596300d0f9cd4476cef9c5d701e6503ad80e7005
AnotheerDev <leger.julien94@gmail.com>
2023-07-21 15:46:45 +0200

    add function index

--- src/Controller/FormateurController.php
+++ src/Controller/FormateurController.php
@@ -2,17 +2,24 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Formateur;
+use App\Entity\Formation;
+use App\Entity\Session;
+use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class FormateurController extends AbstractController
 {
-    #[Route('/formateur', name: 'app_formateur')]
-    public function index(): Response
+    #[Route('/formateur/{id}', name: 'app_formateur')]
+    public function index(ManagerRegistry $doctrine, Formateur $formateur): Response
     {
+        $sessions = $doctrine->getRepository(Session::class)->findAll();
+        // dd($sessions);
         return $this->render('formateur/index.html.twig', [
-            'controller_name' => 'FormateurController',
+            'formateur' => $formateur,
+            'sessions' => $sessions,
         ]);
     }
 }
