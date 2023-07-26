6e3168316c4935d551bfde4842e57feac1461fec
AnotheerDev <leger.julien94@gmail.com>
2023-07-26 15:55:46 +0200

    modify js

--- public/js/script.js
+++ public/js/script.js
@@ -1,9 +1,42 @@
-// // Your JavaScript code
-// const navMenu = document.getElementById('nav-menu');
-// const navToggle = document.getElementById('nav-toggle');
+// /*=============== SHOW MENU ===============*/
+// const navMenu = document.getElementById('nav-menu'),
+//         navToggle = document.getElementById('nav-toggle'),
+//         navClose = document.getElementById('nav-close')
 
-// if (navToggle && navMenu) {
-//     navToggle.addEventListener('click', () => {
-//         navMenu.classList.toggle('show-menu');
-//     });
+// /*===== MENU SHOW =====*/
+// /* Validate if constant exists */
+// if(navToggle){
+//     navToggle.addEventListener('click', () =>{
+//         navMenu.classList.add('show-menu')
+//     })
 // }
+
+// /*===== MENU HIDDEN =====*/
+// /* Validate if constant exists */
+// if(navClose){
+//     navClose.addEventListener('click', () =>{
+//         navMenu.classList.remove('show-menu')
+//     })
+// }
+
+// /*=============== REMOVE MENU MOBILE ===============*/
+// const navLink = document.querySelectorAll('.nav__link')
+
+// const linkAction = () =>{
+//     const navMenu = document.getElementById('nav-menu')
+//     // When we click on each nav__link, we remove the show-menu class
+//     navMenu.classList.remove('show-menu')
+// }
+// navLink.forEach(n => n.addEventListener('click', linkAction))
+
+
+
+/*=============== SHOW / HIDE MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+
+if (navToggle && navMenu) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.toggle('show-menu');
+    });
+}
