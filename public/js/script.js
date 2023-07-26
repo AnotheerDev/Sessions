e11048d9befca74d8577bf02c76053956274814a
AnotheerDev <leger.julien94@gmail.com>
2023-07-26 13:23:07 +0200

    css

---
+++ public/js/script.js
@@ -0,0 +1,9 @@
+// // Your JavaScript code
+// const navMenu = document.getElementById('nav-menu');
+// const navToggle = document.getElementById('nav-toggle');
+
+// if (navToggle && navMenu) {
+//     navToggle.addEventListener('click', () => {
+//         navMenu.classList.toggle('show-menu');
+//     });
+// }
